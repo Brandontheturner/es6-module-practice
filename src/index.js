73152341bf619ceea291f91f684dad1838315095
Brandon Turner <brandontheturner@gmail.com>
2019-03-02 16:15:42 -0600

    modules practice

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import add from "./modules/add.js";
 import subtract from "./modules/subtract.js";
 import multiply from "./modules/multiply.js";
-import {addTen, addFive} from "./modules/addNumbers.js";
-import {subtractOne} from "./modules/subtractNumbers.js";
+import divide from "./modules/divide.js";
+import { addOne, addTen, addFive } from "./modules/addNumbers.js";
+import {
+  subtractOne,
+  subtractFive,
+  subtractTen
+} from "./modules/subtractNumbers.js";
 
 const number1 = 393;
 const number2 = 238;
 
-let answer = add(number1,number2);
+let answer = add(number1, number2);
 console.log(answer);
 
-answer = subtract(number1,number2);
+answer = subtract(number1, number2);
 console.log(answer);
 
-answer = multiply(number1,number2);
+answer = multiply(number1, number2);
 console.log(answer);
 
-answer = divide(number1,number2);
+answer = divide(number1, number2);
 console.log(answer);
 
 answer = addTen(number1);
--- src/modules/addNumbers.js
+++ src/modules/addNumbers.js
@@ -1,7 +1,11 @@
 import add from "./add.js";
 
+export function addOne(num) {
+  return add(num, 1);
+}
+
 export function addTen(num) {
-  return add(num,10);
+  return add(num, 10);
 }
 
 export function addFive(num) {
